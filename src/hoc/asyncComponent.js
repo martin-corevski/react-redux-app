2c6473389d1100a76427ab18243d67967f6db74d
Martin Corevski <martin-corevski@outlook.com>
2018-04-13 16:38:26 +0200

    Async HOC issue with CSS
    https://github.com/webpack-contrib/extract-text-webpack-plugin/issues/456

--- src/hoc/asyncComponent.js
+++ src/hoc/asyncComponent.js
@@ -1,4 +1,14 @@
 import React, { Component } from 'react'
+/**
+ * TODO: Update the configuration when issue is solved
+ * Uncaught (in promise) TypeError: Cannot read property 'call' of undefined...
+ * Error for css file inside async components when using chunks
+ * https://github.com/webpack-contrib/extract-text-webpack-plugin/issues/456
+ * This is a problem for create-react-app also, since it's Webpack problem
+ * https://github.com/facebook/create-react-app/issues/4130
+ */
+require('style-loader/lib/addStyles')
+require('css-loader/lib/css-base')
 
 const asyncComponent = (importComponent) => {
   return class extends Component {
